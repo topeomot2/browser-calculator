197415cef7745aac383eab73ac3f1f19bf61b139
Tope Omotunde <tope@fcolimited.com>
2018-11-02 22:48:19 +0100

    - Added Malformed Expression Identifier - Dealth with decimal point

--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 [![Build Status](https://travis-ci.org/topeomot2/browser-calculator.svg?branch=master)](https://travis-ci.org/topeomot2/browser-calculator)
 
 A Calculator totally hosted in the browser. Patterned after the calculator on an
-Ubuntu system.
+Ubuntu system. The main aim is to validate correct inputs and disallow incorrect ones
+no matter how complex the expression is before doing the final calculation
 
-
+```
+4 + -10 //correct
+(-12 * 4) //correct
+15 / -6 //correct
+32(/16) //wrong
+4 +(14 -(5+17)/(23-13)) //correct
+(5 -(20-9/4) //incorrect
+```
 
 ## Getting Started
 
 This is a calculator totally written in javascript but with the added complexity 
 that brackets are allowed. This brings in added complexity into the mix. It means
-you need to be able to take inputs link the example below.
+you need to be able to take inputs like the example below.
 
 ```
 4 +(14 -(5+17)/(23-13))
--- js/main.js
+++ js/main.js
@@ -25,16 +25,22 @@ function eventListeners() {
 
 function processBtnClick(event) {
 
-    console.log(event.target.tagName);
-    if(event.target.tagName != null && (event.target.tagName.toUpperCase() === 'BUTTON')) {
-        if(event.target.textContent.trim().length > 0) {
-            calculator.addToInput(event.target.textContent.trim());
-        } else {
-            calculator.addToInput(change[event.target.firstChild.classList[1].split('-')[1]]);
+    try{
+        //console.log(event.target.tagName);
+        if(event.target.tagName != null && (event.target.tagName.toUpperCase() === 'BUTTON')) {
+            if(event.target.textContent.trim().length > 0) {
+                calculator.addToInput(event.target.textContent.trim());
+            } else {
+                calculator.addToInput(change[event.target.firstChild.classList[1].split('-')[1]]);
+            }
         }
+        
+        screen.textContent = calculator.getDisplay();
+
+    } catch(e) {
+        document.querySelector('#error_screen').textContent = e.message;
     }
     
-    screen.textContent = calculator.getDisplay();
 
 }
 
